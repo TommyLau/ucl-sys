b5870bc7d47fac3c5266275c085ffd7d63887355
Tommy Lau <tommy.lhg@gmail.com>
2022-03-10 01:14:38 +0800

    Compile UCL in output directory without copy and don't compile every time

--- build.rs
+++ build.rs
@@ -1,11 +1,12 @@
-use std::{env, fs, io, path::PathBuf, process::Command};
-use std::path::Path;
+use std::{env, path::PathBuf, process::Command};
 
 const BINDINGS_FILE: &str = "bindings.rs";
 const WRAPPER_FILE: &str = "wrapper.h";
 
 fn main()
 {
+    println!("cargo:rerun-if-changed=wrapper.h");
+
     let source = PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap());
     let output = PathBuf::from(&env::var("OUT_DIR").unwrap());
 
@@ -16,11 +17,8 @@ fn main()
     // CFLAGS=-Wno-implicit-function-declaration
     println!("cargo:rustc-env=CFLAGS=-Wno-implicit-function-declaration");
 
-    copy_dir_all(source.join("ucl-1.03"), output.join("ucl-1.03"))
-        .expect("Cannot copy ucl-1.03 directory to output");
-
-    Command::new("./configure")
-        .current_dir(&output.join("ucl-1.03"))
+    Command::new(source.join("ucl-1.03/configure"))
+        .current_dir(&output)
         .arg("--disable-debug")
         .arg("--disable-dependency-tracking")
         .arg(&format!("--prefix={}", output.display().to_string()))
@@ -28,7 +26,7 @@ fn main()
         .unwrap();
 
     Command::new("make")
-        .current_dir(&output.join("ucl-1.03"))
+        .current_dir(&output)
         .arg("install")
         .output()
         .unwrap();
@@ -51,18 +49,4 @@ fn main()
     println!("cargo:rustc-link-lib=static={}", name);
     println!("cargo:rustc-link-search=native={}", library);
     println!("cargo:include={}", include);
-}
-
-fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
-    fs::create_dir_all(&dst)?;
-    for entry in fs::read_dir(src)? {
-        let entry = entry?;
-        let ty = entry.file_type()?;
-        if ty.is_dir() {
-            copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
-        } else {
-            fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
-        }
-    }
-    Ok(())
 }
